18f4ba4d50c09bf9a74ae960287f17c8d15f5302
Dan Solovay <dan.solovay@velir.com>
2012-11-17 22:59:21 -0500

    Add DataTransferTests.cs.
    The goal of this test suite is to create a fresh data transfer
    with each test. This will help test changes in how Sitecore
    data is mapped to MongoDB documents.

---
+++ SitecoreData.DataProviders.MongoDB.Tests/DataTransferTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using NUnit.Framework;
+using Sitecore.Configuration;
+
+namespace SitecoreData.DataProviders.MongoDB.Tests
+{
+    [TestFixture]
+    class DataTransferTests
+    {
+        private MongoServer _server;
+        private MongoDatabase _db;
+        [SetUp]
+        public void CopyDataFromTemplatesFolder()
+        {
+            var connectionString = "mongodb://localhost:27017/?safe=true";
+            _server = MongoServer.Create(connectionString);
+            _db = _server.GetDatabase("UnitTesting");
+
+        }
+
+        [TearDown]
+        public void ClearDataBase()
+        { }
+
+        [Test]
+        public void CanMakeConnectionToMongoServer()
+        {
+            Assert.That(_server,Is.Not.Null);
+        }
+
+        [Test]
+        public void CanCreateTestDatabase()
+        {
+            Assert.That(_db, Is.Not.Null);
+        }
+
+        [Test]
+        public void CanCopyDataToTestDatabase()
+        {
+            
+        }
+
+        [Test]
+        public void CanAccessSitecoreDatabase()
+        {
+            Assert.That(Factory.GetDatabase("master"), Is.Not.Null);
+        }
+
+
+
+    }
+}
