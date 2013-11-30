af60bd2fcd3745ff71d40d862e1b0088dbc3cc2c
Dan Solovay <dan.solovay@velir.com>
2013-11-29 21:59:27 -0500

    Write failing test for root node field transfer.

--- tst/SitecoreData.DataProviders.MongoDB.Tests/DataTransferTests.cs
+++ tst/SitecoreData.DataProviders.MongoDB.Tests/DataTransferTests.cs
@@ -10,66 +10,75 @@ using Sitecore.Data.Items;
 
 namespace SitecoreData.DataProviders.MongoDB.Tests
 {
-    [TestFixture]
-    [Category("MongoDB Data Transfer Tests")]
-    class DataTransferTests : MongoTestsBase
-    {
+	[TestFixture]
+	[Category("MongoDB Data Transfer Tests")]
+	internal class DataTransferTests : MongoTestsBase
+	{
 
-        [TearDown]
-        public void ClearDataBase()
-        {
-            _db.Drop();
-        }
-        [Test]
-        public void CanCreateTestDatabase()
-        {
-            Assert.That(_db, Is.Not.Null);
-        }
+		[TearDown]
+		public void ClearDataBase()
+		{
+			_db.Drop();
+		}
 
-        [Test]
-        public void MongoDbHasItemsCollection()
-        {
-            TransferUtil.TransferPath("/sitecore/layout", _sourceDatabase, _targetDatabase, null);
-            Assert.That(_db.GetCollectionNames().Contains("items"), Is.True);
-        }
+		[Test]
+		public void CanCreateTestDatabase()
+		{
+			Assert.That(_db, Is.Not.Null);
+		}
 
-        [Test]
-        public void CanTransferData()
-        {
-            TransferUtil.TransferPath("/sitecore/layout", _sourceDatabase, _targetDatabase, null);
-            Assert.That(_db.GetCollection("items").Count(), Is.EqualTo(60));
-        }
+		[Test]
+		public void MongoDbHasItemsCollection()
+		{
+			TransferUtil.TransferPath("/sitecore/layout", _sourceDatabase, _targetDatabase, null);
+			Assert.That(_db.GetCollectionNames().Contains("items"), Is.True);
+		}
 
-        [Test]
-        public void CanTransferDeepHierarchy()
-        {
-            TransferUtil.TransferPath("/sitecore/layout/renderings", _sourceDatabase, _targetDatabase, null);
-            Assert.That(_db.GetCollection("items").Count(), Is.EqualTo(13));
-        }
+		[Test]
+		public void CanTransferData()
+		{
+			TransferUtil.TransferPath("/sitecore/layout", _sourceDatabase, _targetDatabase, null);
+			Assert.That(_db.GetCollection("items").Count(), Is.EqualTo(60));
+		}
 
-        [Test]
-        public void CanAccessSitecoreLayoutItem()
-        {
-            Item item = Factory.GetDatabase("master").GetItem("/sitecore/layout");
-            Assert.That(item.ID.ToString(), Is.EqualTo("{EB2E4FFD-2761-4653-B052-26A64D385227}"));
-        }
+		[Test]
+		public void CanTransferDeepHierarchy()
+		{
+			TransferUtil.TransferPath("/sitecore/layout/renderings", _sourceDatabase, _targetDatabase, null);
+			Assert.That(_db.GetCollection("items").Count(), Is.EqualTo(13));
+		}
 
-        [Test]
-        public void CallbackMethodCalled()
-        {
-            var action = A.Fake<Action<string>>();
-            using (var scope = Fake.CreateScope())
-            {
-                TransferUtil.TransferPath("/sitecore/layout/devices/print", _sourceDatabase, _targetDatabase, action);
-                using (scope.OrderedAssertions())
-                {
-                    A.CallTo(() => action.Invoke("/sitecore")).MustHaveHappened(Repeated.Exactly.Once);
-                    A.CallTo(() => action.Invoke("/sitecore/layout")).MustHaveHappened(Repeated.Exactly.Once);
-                    A.CallTo(() => action.Invoke("/sitecore/layout/Devices")).MustHaveHappened(Repeated.Exactly.Once);
-                    A.CallTo(() => action.Invoke("/sitecore/layout/Devices/Print")).MustHaveHappened(
-                        Repeated.Exactly.Once);
-                }
-            }
-        }
-    }
+		[Test]
+		public void CanAccessSitecoreLayoutItem()
+		{
+			Item item = Factory.GetDatabase("master").GetItem("/sitecore/layout");
+			Assert.That(item.ID.ToString(), Is.EqualTo("{EB2E4FFD-2761-4653-B052-26A64D385227}"));
+		}
+
+		[Test]
+		public void CallbackMethodCalled()
+		{
+			var action = A.Fake<Action<string>>();
+			using (var scope = Fake.CreateScope())
+			{
+				TransferUtil.TransferPath("/sitecore/layout/devices/print", _sourceDatabase, _targetDatabase, action);
+				using (scope.OrderedAssertions())
+				{
+					A.CallTo(() => action.Invoke("/sitecore")).MustHaveHappened(Repeated.Exactly.Once);
+					A.CallTo(() => action.Invoke("/sitecore/layout")).MustHaveHappened(Repeated.Exactly.Once);
+					A.CallTo(() => action.Invoke("/sitecore/layout/Devices")).MustHaveHappened(Repeated.Exactly.Once);
+					A.CallTo(() => action.Invoke("/sitecore/layout/Devices/Print")).MustHaveHappened(
+						Repeated.Exactly.Once);
+				}
+			}
+		}
+
+		[Test]
+		public void TranferUtil_WhenTransferingRoot_PopulatesFields()
+		{
+			TransferUtil.TransferPath("/sitecore/content", _sourceDatabase, _targetDatabase, null);
+			Item root = _targetDatabase.GetItem("/sitecore");
+			Assert.IsNotEmpty(root["__security"]);
+		}
+	}
 }
